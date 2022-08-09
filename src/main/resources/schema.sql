35886fa7ddef1ad84c9f7b73368e5e41509f85fb
yglee730 <yglee730@naver.com>
2022-08-09 16:03:40 +0900

    db schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,31 @@
-CREATE TABLE user(
-    userID INT,
+CREATE TABLE users(
+    userID INT PRIMARY KEY,
     userName VARCHAR(255),
     userPass VARCHAR(255),
     userEmail VARCHAR(255),
     userTel VARCHAR(255)
+);
+
+CREATE TABLE metaSurvey(
+    surveyID INT PRIMARY KEY,
+    surveyName VARCHAR(255),
+    surveyWriter VARCHAR(255),
+    writeDate DATETIME 
+);
+
+CREATE TABLE contentSurvey(
+    surveyID INT,
+    questionNum INT,
+    questionContent VARCHAR(255),
+    questionType VARCHAR(128),
+
+    FOREIGN KEY (surveyID) REFERENCES metaSurvey (surveyID)
+);
+
+CREATE TABLE userResponse(
+    surveyID INT,
+    questionNum INT,
+    ResponseContent VARCHAR(512),
+
+    FOREIGN KEY (surveyID) REFERENCES metaSurvey(surveyID)
 );
